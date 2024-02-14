aed4fc187857ac75511692b6feb3ad3cb051e2df
Olaf Booij (at TUD) <olafbooij@bestprima.nl>
2024-02-14 19:42:19 +0100

    test is ready

--- experiments/discetize_time/simple_net.py
+++ experiments/discetize_time/simple_net.py
@@ -1,5 +1,6 @@
 
 import cupy as cp
+from math import exp
 import numpy as np
 
 from bats.Layers import InputLayer, LIFLayer
@@ -38,22 +39,16 @@ if __name__ == "__main__":
 
     loss_fct = SpikeTimeLoss()
 
-    # one step
-    output_layer.weights = np.array([[4.]])
-    print(f"weight    = {output_layer.weights[0, 0]}")
+    weight = 4.
+    output_layer.weights = np.array([[weight]])
     network.forward(SPIKE_TIMES, N_SPIKE_TIMES, max_simulation=SIMULATION_TIME)
     out_spikes, n_out_spikes = network.output_spike_trains
-    print(f"spiketime = {out_spikes[0, 0, 0]}")
-    loss, errors = loss_fct.compute_loss_and_errors(out_spikes, n_out_spikes, LABELS_GPU)
-    print(f"loss      = {loss[0]}")
-    gradient = network.backward(errors)
-    print(f"gradient  = {gradient[1][0, 0, 0]}")
+    _loss, errors = loss_fct.compute_loss_and_errors(out_spikes, n_out_spikes, LABELS_GPU)
+    spiketime = out_spikes[0, 0, 0]
+    gradient = network.backward(errors)[1][0, 0, 0]
 
-    #s_m = exp(-spiketime / 2)
-    #s_s = exp(- spiketime / 1)
-    #dtdw = - (s_m - s_s) / (weight * (-s_m / 2 + s_s))
-    #dldt * dtdw * .1
-    ##loss = spiketime ^ 2 / 2
-    #dldt = spiketime
-    #== gradient
+    s_m = exp(-spiketime / 2)
+    s_s = exp(-spiketime / 1)
+    dtdw = - (s_m - s_s) / (weight * (-s_m / 2 + s_s))
+    assert(abs(spiketime * dtdw - gradient) < 1e-6)
 
